d8133b6eecd14902fbc3f5a625c671058ca7122a
maikytech <maikytech1307@gmail.com>
2020-06-09 16:41:29 -0500

    Start Project

---
+++ Business-Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Business-Card
+//
+//  Created by Maiqui Cedeño on 9/06/20.
+//  Copyright © 2020 PosetoStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
