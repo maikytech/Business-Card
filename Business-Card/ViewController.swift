5483a8f4064c571cefd507313324126d3655fbcb
maikytech <maikytech1307@gmail.com>
2020-06-18 12:31:56 -0500

    Add label name profile

--- Business-Card/ViewController.swift
+++ Business-Card/ViewController.swift
@@ -10,22 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    let namePerson = UILabel()
     let profileImage = UIImageView()
     var constraintsToApply = [NSLayoutConstraint]()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         createAndLoadImage(imageName: "profileMaiky")
+        self.view.layoutIfNeeded()
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         
         NSLayoutConstraint.activate(constraintsToApply)
-            
     }
     
     func createAndLoadImage(imageName : String) {
@@ -45,6 +44,23 @@ class ViewController: UIViewController {
         constraintsToApply.append(centerYConstraint)
         constraintsToApply.append(withConstraint)
         constraintsToApply.append(heightConstraint)
+        
+        profileImage.layer.cornerRadius = withConstraint.constant / 2.0
+    }
+    
+    func nameLabelConfiguration(name: String) {
+        
+        namePerson.text = name
+        namePerson.textColor = UIColor.black
+        namePerson.translatesAutoresizingMaskIntoConstraints = false
+        namePerson.textAlignment = NSTextAlignment.center
+        namePerson.font = UIFont(name: "Avenir Next", size: 20)
+        self.view.addSubview(namePerson)
+        
+        let leadingConstraint = namePerson.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0)
+        let trailingConstraint = namePerson.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0)
+        let topConstraint = namePerson.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8)
+        let heightConstraint = namePerson.heightAnchor.constraint(equalToConstant: 25)
     }
 }
 
