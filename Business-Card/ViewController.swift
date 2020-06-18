aa1975060c7f71802088ec9327413a5b0ca4c040
maikytech <maikytech1307@gmail.com>
2020-06-17 21:36:12 -0500

    Add profile image

--- Business-Card/ViewController.swift
+++ Business-Card/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let profileImage = UIImageView()
+    var constraintsToApply = [NSLayoutConstraint]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createAndLoadImage(imageName: "profileMaiky")
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        NSLayoutConstraint.activate(constraintsToApply)
+            
+    }
+    
+    func createAndLoadImage(imageName : String) {
+        
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.image = UIImage(named: imageName)
+        profileImage.contentMode = UIView.ContentMode.scaleAspectFill
+        profileImage.clipsToBounds = true
+        self.view.addSubview(profileImage)
+        
+        let centerXConstraint = profileImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0)
+        let centerYConstraint = profileImage.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: 0)
+        let withConstraint = profileImage.widthAnchor.constraint(equalToConstant: 175)
+        let heightConstraint = profileImage.heightAnchor.constraint(equalToConstant: 175)
+        
+        constraintsToApply.append(centerXConstraint)
+        constraintsToApply.append(centerYConstraint)
+        constraintsToApply.append(withConstraint)
+        constraintsToApply.append(heightConstraint)
+    }
 }
 
